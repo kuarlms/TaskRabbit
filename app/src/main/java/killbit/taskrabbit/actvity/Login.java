4bd0f59fcc27731d9c421627deab9394bde185f9
kural <kural.ms@claritaz.com>
2017-10-13 17:47:33 +0530

    Fb login In

--- app/src/main/java/killbit/taskrabbit/actvity/Login.java
+++ app/src/main/java/killbit/taskrabbit/actvity/Login.java
@@ -13,6 +13,8 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
@@ -25,6 +27,12 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -58,7 +66,7 @@ public class Login extends FragmentActivity implements GoogleApiClient.OnConnect
     openssl sha1 -binary | openssl base64
 
     on Windows run this command into Terminal:
-       
+
     keytool -exportcert -alias androiddebugkey -keystore %HOMEPATH%\.android\debug.keystore |
     openssl sha1 -binary | openssl base64*/
 
@@ -98,6 +106,8 @@ public class Login extends FragmentActivity implements GoogleApiClient.OnConnect
             public void onSuccess(LoginResult loginResult) {
                 Log.d(TAG, "facebook:onSuccess:" + loginResult);
                 handleFacebookAccessToken(loginResult.getAccessToken());
+
+
             }
 
             @Override
@@ -126,8 +136,56 @@ public class Login extends FragmentActivity implements GoogleApiClient.OnConnect
 
     private void handleFacebookAccessToken(AccessToken accessToken) {
         Toast.makeText(this, ""+accessToken.getUserId(), Toast.LENGTH_SHORT).show();
+        GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
 
 
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                Bundle bFacebookData = getFacebookData(object);
+                Log.d("fb",object.toString());
+
+            }});
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id, first_name, last_name, email,gender, birthday, location"); // Parámetros que pedimos a facebook
+        request.setParameters(parameters);
+        request.executeAsync();
+
+        }
+    private Bundle getFacebookData(JSONObject object) {
+
+        try {
+            Bundle bundle = new Bundle();
+            String id = object.getString("id");
+
+            try {
+                URL profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?width=200&height=150");
+                Log.i("profile_pic", profile_pic + "");
+                bundle.putString("profile_pic", profile_pic.toString());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            bundle.putString("idFacebook", id);
+            if (object.has("first_name"))
+                bundle.putString("first_name", object.getString("first_name"));
+            if (object.has("last_name"))
+                bundle.putString("last_name", object.getString("last_name"));
+            if (object.has("email"))
+                bundle.putString("email", object.getString("email"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+            if (object.has("birthday"))
+                bundle.putString("birthday", object.getString("birthday"));
+            if (object.has("location"))
+                bundle.putString("location", object.getJSONObject("location").getString("name"));
+
+            return bundle;
+        } catch (JSONException e) {
+            Log.d(TAG, "Error parsing JSON");
+        }
+        return null;
     }
 
 
