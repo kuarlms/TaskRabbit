91fab2fc01e7dcc6a7e99978a37fee8022954df8
kural <kural.ms@claritaz.com>
2017-11-20 19:18:54 +0530

    20 nov tasker adp update..

--- app/src/main/java/killbit/taskrabbit/adapters/tasker_details_adapter.java
+++ app/src/main/java/killbit/taskrabbit/adapters/tasker_details_adapter.java
@@ -6,8 +6,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -27,8 +31,17 @@ public class tasker_details_adapter extends RecyclerView.Adapter<tasker_details_
     OnRecyclerListener recyclerListener;
 
 
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView tv_month, tv_day,tv_date;
+        TextView tv_adp_tasker_name, tv_adp_tasker_review,
+                tv_adp_tasker_iv_verifed,tv_adp_tasker_tearms_txt_description
+                ,tv_adp_tasker_since,tv_adp_tasker_number_of_tasks,
+                tv_adp_tasker_quick_notes,tv_adp_tasker_details1,
+                tv_adp_tasker_details2,tv_adp_tasker_details3,
+                tv_adp_tasker_pleople_said;
+        RecyclerView rv_tsker_reviews;
+        ImageView iv_taker_pic;
+        Button btn_tasker_adp_rate;
         LinearLayout item_parent;
 
 
@@ -36,11 +49,20 @@ public class tasker_details_adapter extends RecyclerView.Adapter<tasker_details_
         public MyViewHolder(View view, Context context) {
             super(view);
 
-            /*tv_month =view.findViewById(R.id.txt_adp_month);
-            tv_day =view.findViewById(R.id.txt_adp_day);
-            tv_date = view.findViewById(R.id.txt_adp_date);
-            item_parent = view.findViewById(R.id.parent_item_adp_home);
-*/
+            iv_taker_pic = view.findViewById(R.id.iv_tasker_details_adp);
+            tv_adp_tasker_name = view.findViewById(R.id.tv_adp_tasker_name);
+            tv_adp_tasker_review = view.findViewById(R.id.tv_adp_tasker_review);
+            tv_adp_tasker_iv_verifed = view.findViewById(R.id.tv_adp_tasker_iv_verifed);
+            tv_adp_tasker_tearms_txt_description= view.findViewById(R.id.tv_adp_tasker_tearms_txt_description);
+            tv_adp_tasker_since= view.findViewById(R.id.tv_adp_tasker_since);
+            tv_adp_tasker_number_of_tasks= view.findViewById(R.id.tv_adp_tasker_number_of_tasks);
+            tv_adp_tasker_quick_notes= view.findViewById(R.id.tv_adp_tasker_quick_notes);
+            tv_adp_tasker_details1= view.findViewById(R.id.tv_adp_tasker_details1);
+            tv_adp_tasker_details2= view.findViewById(R.id.tv_adp_tasker_details2);
+            tv_adp_tasker_details3= view.findViewById(R.id.tv_adp_tasker_details3);
+            tv_adp_tasker_pleople_said= view.findViewById(R.id.tv_adp_tasker_pleople_said);
+            btn_tasker_adp_rate = view.findViewById(R.id.btn_tasker_adp_rate);
+
 
         }
     }
@@ -66,24 +88,42 @@ public class tasker_details_adapter extends RecyclerView.Adapter<tasker_details_
     @Override
     public void onBindViewHolder( MyViewHolder holder,  int position) {
         tasker_list_data listData = ListDatas.get(position);
-      /*  holder.tv_month.setText(listData.getMonth());
-        holder.tv_day.setText(listData.getDay());
-        holder.tv_date.setText(listData.getDate());*/
+        Glide.with(context).load(R.drawable.a).into(holder.iv_taker_pic);
+        holder.tv_adp_tasker_name.setText(listData.getFirstName());
+       /* iv_taker_pic = view.findViewById(R.id.iv_tasker_details_adp);
+
+        tv_adp_tasker_iv_verifed = view.findViewById(R.id.tv_adp_tasker_iv_verifed);
+        tv_adp_tasker_tearms_txt_description= view.findViewById(R.id.tv_adp_tasker_tearms_txt_description);
+        tv_adp_tasker_since= view.findViewById(R.id.tv_adp_tasker_since);
+        tv_adp_tasker_number_of_tasks= view.findViewById(R.id.tv_adp_tasker_number_of_tasks);
+        tv_adp_tasker_quick_notes= view.findViewById(R.id.tv_adp_tasker_quick_notes);
+        tv_adp_tasker_details1= view.findViewById(R.id.tv_adp_tasker_details1);
+        tv_adp_tasker_details2= view.findViewById(R.id.tv_adp_tasker_details2);
+        tv_adp_tasker_details3= view.findViewById(R.id.tv_adp_tasker_details3);
+        tv_adp_tasker_pleople_said= view.findViewById(R.id.tv_adp_tasker_pleople_said);
+        btn_tasker_adp_rate = view.findViewById(R.id.btn_tasker_adp_rate);*/
+       holder.tv_adp_tasker_review.setText(listData.getReviewResponseRate());
+       holder.tv_adp_tasker_iv_verifed.setText(listData.getIdVerified());
+       holder.tv_adp_tasker_tearms_txt_description.setText(listData.getAbout());
 
 
-        holder.item_parent.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+    /*    holder.item_parent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
               //  String Cda = listData.getMonth()+" , "+listData.getDay()+" , "+listData.getDate()+" ";
-                //recyclerListener.onItemClicked(position, Cda);
+                recyclerListener.onTaskerSelected(position, listData.getFirstName());
             }
-        });
+        });*/
 
 
     }
 
     public interface OnRecyclerListener {
-        void onItemClicked(int position, String data);
+        void onTaskerSelected(int position, String data);
 
     }
 
