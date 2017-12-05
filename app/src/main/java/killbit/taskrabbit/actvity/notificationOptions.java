5db560fed113ef83e7523c720f5c1b5527888280
kural mughil selvam <kurslms@gmail.com>
2017-12-06 01:20:53 +0530

    6 dec tasker adp update..

---
+++ app/src/main/java/killbit/taskrabbit/actvity/notificationOptions.java
@@ -0,0 +1,133 @@
+package killbit.taskrabbit.actvity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import killbit.taskrabbit.R;
+import killbit.taskrabbit.retrofit.ApiInterface;
+import killbit.taskrabbit.retrofit.ApiUtils;
+import killbit.taskrabbit.retrofit.StatusResp;
+import killbit.taskrabbit.utils.sp_task;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by kural on 10/10/17.
+ */
+
+public class notificationOptions extends Activity {
+        String email,
+                str_cb_email,str_cb_sms;
+
+    SharedPreferences sp;
+    SharedPreferences.Editor  editor ;
+
+
+
+    @BindView(R.id.checkbox_email)
+    CheckBox cb_email;
+
+    @BindView(R.id.checkbox_sms)
+    CheckBox cb_sms;
+
+
+    @BindView(R.id.tb_normal_title)
+    TextView tv_title;
+
+
+
+    ApiInterface mAPIService;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_notification_options);
+        ButterKnife.bind(this);
+
+         mAPIService = ApiUtils.getAPIService();
+        sp =  getSharedPreferences(sp_task.MyPref, Context.MODE_PRIVATE);
+        editor =sp.edit();
+        tv_title.setText("Notification");
+
+        email = sp.getString(sp_task.Sp_email,"");
+
+
+    }
+
+    @OnClick(R.id.button_change_noti)
+    public void setBtn_confirm(){
+
+
+
+        if(cb_email.isChecked() ){
+
+            str_cb_email = "1";
+
+        }else {
+
+            str_cb_email = "0";
+
+        }
+
+        if(cb_sms.isChecked() ){
+
+            str_cb_sms = "1";
+
+        }else {
+
+            str_cb_sms = "0";
+
+        }
+
+
+        mAPIService.rf_Notification(ApiInterface.header_value, email,"save",str_cb_email,str_cb_sms)
+                .enqueue(new Callback<StatusResp>() {
+                    @Override
+                    public void onResponse(Call<StatusResp> call, Response<StatusResp> response) {
+                        if(response.body().getStatus().equals(1)){
+                            Toast.makeText(notificationOptions.this, ""+response.body().getMessage(), Toast.LENGTH_SHORT).show();
+
+                            finish();
+                        }else {
+                            Toast.makeText(notificationOptions.this, ""+response.body().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<StatusResp> call, Throwable t) {
+
+                        Toast.makeText(notificationOptions.this, ""+t, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+    }
+
+    @OnClick({R.id.tb_normal_back})
+    public void tb_back(){
+
+        finish();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/killbit/taskrabbit/retrofit/transactionList/ExportResult.java
@@ -0,0 +1,101 @@
+
+package killbit.taskrabbit.retrofit.transactionList;
+
+import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ExportResult implements Serializable
+{
+
+    @SerializedName("takser_name")
+    @Expose
+    private String takserName;
+    @SerializedName("task_name")
+    @Expose
+    private String taskName;
+    @SerializedName("pro_pic")
+    @Expose
+    private String proPic;
+    @SerializedName("booking_date")
+    @Expose
+    private String bookingDate;
+    @SerializedName("booking_day")
+    @Expose
+    private String bookingDay;
+    @SerializedName("booking_month")
+    @Expose
+    private String bookingMonth;
+    @SerializedName("currency_symbol")
+    @Expose
+    private String currencySymbol;
+    @SerializedName("paid_amount")
+    @Expose
+    private String paidAmount;
+    private final static long serialVersionUID = 3410349639409297894L;
+
+    public String getTakserName() {
+        return takserName;
+    }
+
+    public void setTakserName(String takserName) {
+        this.takserName = takserName;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getProPic() {
+        return proPic;
+    }
+
+    public void setProPic(String proPic) {
+        this.proPic = proPic;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public String getBookingDay() {
+        return bookingDay;
+    }
+
+    public void setBookingDay(String bookingDay) {
+        this.bookingDay = bookingDay;
+    }
+
+    public String getBookingMonth() {
+        return bookingMonth;
+    }
+
+    public void setBookingMonth(String bookingMonth) {
+        this.bookingMonth = bookingMonth;
+    }
+
+    public String getCurrencySymbol() {
+        return currencySymbol;
+    }
+
+    public void setCurrencySymbol(String currencySymbol) {
+        this.currencySymbol = currencySymbol;
+    }
+
+    public String getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(String paidAmount) {
+        this.paidAmount = paidAmount;
+    }
+
+}
