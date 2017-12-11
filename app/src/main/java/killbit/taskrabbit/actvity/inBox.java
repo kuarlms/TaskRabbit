d420368955ceada116060760d2362ef4ed289aca
kural <kural.ms@claritaz.com>
2017-12-11 19:29:34 +0530

    Rabbit New APIs

--- app/src/main/java/killbit/taskrabbit/actvity/inBox.java
+++ app/src/main/java/killbit/taskrabbit/actvity/inBox.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
@@ -15,21 +17,25 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import killbit.taskrabbit.R;
-import killbit.taskrabbit.adapters.active_tasks_adp;
-import killbit.taskrabbit.objects.active_tasks_data;
+import killbit.taskrabbit.adapters.inbox_adapter;
 import killbit.taskrabbit.retrofit.ApiInterface;
 import killbit.taskrabbit.retrofit.ApiUtils;
+import killbit.taskrabbit.retrofit.inbox.InboxResp;
+import killbit.taskrabbit.retrofit.inbox.MessageList;
 import killbit.taskrabbit.utils.sp_task;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by kural on 11/10/17.
  */
 
-public class inBox extends FragmentActivity{
+public class inBox extends FragmentActivity implements  inbox_adapter.OnRecyclerListener{
 
-    active_tasks_data tasks_data;
-    List<active_tasks_data> tasks_datas = new ArrayList<>();
-    active_tasks_adp adapter_act_tsk;
+    MessageList data;
+    List<MessageList> Listdata = new ArrayList<>();
+    inbox_adapter adapter;
 
     @BindView(R.id.recycleView_tasker_details)
     RecyclerView rv_at_list;
@@ -48,7 +54,7 @@ public class inBox extends FragmentActivity{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tasker_recycler);
-       ButterKnife.bind(this);
+        ButterKnife.bind(this);
         mAPIService = ApiUtils.getAPIService();
         sp =  getSharedPreferences(sp_task.MyPref, Context.MODE_PRIVATE);
         editor =sp.edit();
@@ -56,27 +62,11 @@ public class inBox extends FragmentActivity{
         tv_title.setText("Inbox");
         email = sp.getString(sp_task.Sp_email,"");
 
-/*
-        for (int i = 0; i < 12; i++) {
-            tasks_data = new active_tasks_data(""+i,
-                    ""+i,
-                    ""+i,
-                    ""+i,
-                    ""+i,
-                    ""+i,
-                    ""+i,
-                    ""+i
-                    );
-
-
-            tasks_datas.add(tasks_data);
-        }
-
-        adapter_act_tsk = new active_tasks_adp(tasks_datas);
+        adapter = new inbox_adapter(Listdata,this,inBox.this);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         rv_at_list.setLayoutManager(mLayoutManager);
         rv_at_list.setItemAnimator(new DefaultItemAnimator());
-        rv_at_list.setAdapter(adapter_act_tsk);*/
+        rv_at_list.setAdapter(adapter);
 
 
 
@@ -86,6 +76,49 @@ public class inBox extends FragmentActivity{
     public void tb_back(){
 
         finish();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAPIService.rf_inbox(ApiInterface.header_value, sp.getString(sp_task.Sp_email,"")).enqueue(new Callback<InboxResp>() {
+
+
+
+            @Override
+            public void onResponse(Call<InboxResp> call, Response<InboxResp> response) {
+                if(response.body().getStatus().equals(1)){
+                    for (int i = 0; i <response.body().getMessageList().size() ; i++) {
+
+                        data = new MessageList(response.body().getMessageList().get(i).getName(),
+                                response.body().getMessageList().get(i).getProfileImage(),
+                                response.body().getMessageList().get(i).getTaskName(),
+                                response.body().getMessageList().get(i).getCreatedTime(),
+                                response.body().getMessageList().get(i).getMessage(),
+                                response.body().getMessageList().get(i).getBookingId());
+
+                        Listdata.add(data);
+                        adapter.notifyDataSetChanged();
+                    }
+
+
+                }else {
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<InboxResp> call, Throwable t) {
+
+            }  });
+
+
+    }
+
+    @Override
+    public void onInboxItemSelected(int position, String tasker_id, String Profile_pic, String RatePerHr, String TaskerName) {
 
     }
 }
